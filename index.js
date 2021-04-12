4427e10fa855eb558660a0900b4a9fda9c04eaa8
Michael <mlarock711@comcast.net>
2021-04-11 22:45:27 -0400

    pseudo code to breakdown my thoughts

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function calculate(numberOne, operation, numberTwo) {
+/* eslint-disable no-console */
+
+
+// make function called calculate
+// have null indexies equate to 0.
+// make stringfield number strings Type coercion to number Types.
+// loop through array and remove non-numbers from it.
+// return equation values from switch statement.
+
+const calculate = (numberOne, operation, numberTwo) => {
   switch (operation) {
     case '+':
       return numberOne + numberTwo
@@ -9,8 +18,10 @@ function calculate(numberOne, operation, numberTwo) {
     case '/':
       return numberOne / numberTwo
     default:
-      return 'Operation not yet supported. You sent ' + operation
+    // invalid data point return NAN  
+    return NaN
   }
 }
-console.log(calculate(5 + 5))
+
+console.log(calculate(4 + 5))
 module.exports = { calculate }
