04a6ec14a3081b0499ca506d1e2e044ba2372fdf
Michael <mlarock711@comcast.net>
2021-04-13 17:29:25 -0400

    removed pseudo code

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-/* eslint-disable no-console */
-// make function called calculate
+
 const calculate = (arr) => {
-  // two elements or less than 3 should be NaN
-
-
   arr.length
   let arr2 = []
   let operand = ['+', '-', '*', '/']
 
-  // remove non-numbers or non-operators from arr.
+
   arr.forEach((item) => {
     if (typeof item === 'string' || typeof item === 'number' || item === null) {
       if (operand.includes(item)) {
@@ -21,42 +17,15 @@ const calculate = (arr) => {
         }
       }
     }
-    console.log(item)
   })
 
 
 
-
-
-
-  // loop trough the array to pick out on valid numbers and operator in a new array 
-
-  // if (item[i] === null) {
-  // return item === 0
-  // }
-
-
-  // if (arr.length < 3) {
-  // return NaN
-  // }
-
-  // newarr.number and newarr.operator
-
-  // if it is three go through switch statement 
-
-  // have null indexies equate to 0.
-  // if operation includes (extra)
-  // return true
-
-  // if operation isNAN (extra)
-  // return false
-
-  // make stringfield number strings Type coercion to number Types.
   let numberOne = Number(arr2[0])
   let numberTwo = Number(arr2[2])
   let operation = arr2[1]
 
-  // return equation values from switch statement.
+
   switch (operation) {
     case '+':
       return numberOne + numberTwo
@@ -67,7 +36,6 @@ const calculate = (arr) => {
     case '/':
       return numberOne / numberTwo
     default:
-    // invalid data point return NAN  
       return NaN
   }
 }
